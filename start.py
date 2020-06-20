a5669fadbed3b8d997c60f12f1b6df537d5b4ee9
Will Dunlop <54122840+wjdunlop@users.noreply.github.com>
2020-06-20 11:46:07 -0700

    Update start.py

--- start.py
+++ start.py
@@ -1,6 +1,39 @@
+import sys
+from os import system, name
+
 print('#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#')
 print('# Killa Trumpz text-based adventure #')
 print('#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#')
 
+_BASESP = 10
 
 
+def clearScreen():
+
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
+
+def chooseCharacter():
+    sp = _BASESP
+    print("=========CHARACTER CUSTOMIZATION=========")
+    
+    while sp > 0:
+        
+        print("Customize your character! you have >>"+str(sp)+"<< skill points remaining...")
+
+        sp -= 1
+
+
+    
+
+    clearScreen()
+    
+    return 8
+
+chooseCharacter()
+print('cat')
